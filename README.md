3afc4c3d3f1719c5b877a401ff1d380883fd1d6b
Danilo Cianfrone <danilocianfr@gmail.com>
2024-06-05 02:15:33 +0200

    feat: first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# openmoto kontroller
+
+An open-source, ESP32-C3-based motorcycle navigation controller - 3D-printable and to assemble at home.
+
+`kontroller` features:
+
+* 4-way directional pad
+* 2 Fn buttons
+* 2-way toggle switch
+* Status LED
+
+## Quick Links
+
+- [OnShape CAD project](https://cad.onshape.com/documents/05acf8be03822f2e6aff9547/w/6537d666d6311c590d655c9e/e/7ea95f3bc776815a7c85dc2a)
---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "openmoto kontroller devenv";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    esp-dev = {
+      url = "github:mirrexagon/nixpkgs-esp-dev";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs = { nixpkgs, flake-utils, fenix, esp-dev, ... }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlays = [
+            fenix.overlays.default
+            esp-dev.overlays.default
+          ];
+
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+
+          rustToolchain = fenix.packages.${system}.fromToolchainFile {
+            file = ./rust-toolchain.toml;
+            sha256 = "sha256-Fj+OcOTyexYiW/3M1X1YkNJ/tnuHStX/meU7MHC3AxY=";
+          };
+        in
+        {
+          devShells.default = with pkgs; mkShell {
+            packages = [
+              nil
+              nixpkgs-fmt
+            ];
+
+            buildInputs = [
+              openssl
+              pkg-config
+              rustToolchain
+              cargo-generate
+              cargo-espflash
+
+              esp-idf-esp32c3
+            ];
+
+            shellHook = ''
+              export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [ pkgs.libxml2 pkgs.zlib pkgs.stdenv.cc.cc.lib ]}"
+              export LIBCLANG_PATH=${pkgs.libclang.lib}/lib
+            '';
+          };
+        }
+      );
+}
